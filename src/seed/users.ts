07691d50dd9a084d0956932a91b6349cbb60a916
Oleksandr Prokopenko <prokopenkojs@gmail.com>
2025-05-01 17:28:39 +0300

    test: Add seed for users

---
+++ src/seed/users.ts
@@ -0,0 +1,19 @@
+import { db, dbAsync, TABLES } from "../db.ts";
+import type { UserSensitive } from "../entities/user1.ts";
+
+const user: UserSensitive = {
+  id: 1,
+  email: "user@email.com",
+  password: "abcd",
+};
+
+export async function seedUsers() {
+  await dbAsync.run(
+    `INSERT INTO ${TABLES.USERS} (email, password) VALUES (?, ?)`,
+    [user.email, user.password],
+  );
+}
+
+export async function clearSharedTasks() {
+  db.run(`DELETE FROM ${TABLES.USERS}`);
+}
